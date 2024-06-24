e3f0d5de761924776353b654471d3791e4ddca1a
jw-park-github <jw.park.github@gmail.com>
2024-06-24 18:36:09 +0900

    heroku url

--- src/main/java/com/devdoc/backend/config/WebMvcConfig.java
+++ src/main/java/com/devdoc/backend/config/WebMvcConfig.java
@@ -14,7 +14,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // 모든 경로에 대해 CORS 설정 적용
-                .allowedOrigins("http://localhost:3000") // 허용할 오리진 설정
+                .allowedOrigins("http://localhost:3000","https://devdoc-backend-a9c04f690ebd.herokuapp.com") // 허용할 오리진 설정
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS") // 허용할 HTTP 메서드 설정
                 .allowedHeaders("*") // 모든 헤더 허용
                 .allowCredentials(true) // 인증 정보 허용
